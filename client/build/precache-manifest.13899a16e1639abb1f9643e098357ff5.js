dda6a16d5935bec4432c0bcde7a786b805d2d0b5
Massimo Guida <massimo@massimoguida.com>
2020-07-09 23:50:01 -0400

    yarn build for deployment and added link url to readme

---
+++ client/build/precache-manifest.13899a16e1639abb1f9643e098357ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e3f7ea7767d604e5913",
+    "url": "/static/css/main.b4bdb799.chunk.css"
+  },
+  {
+    "revision": "4e3f7ea7767d604e5913",
+    "url": "/static/js/main.b6f46c65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7b17ce29e17d2f072a13",
+    "url": "/static/js/2.385e1e11.chunk.js"
+  },
+  {
+    "revision": "758e6672ec808b452d79b6af0d927705",
+    "url": "/index.html"
+  }
+];
