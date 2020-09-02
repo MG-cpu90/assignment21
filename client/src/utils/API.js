82f4d177ea482f3530261f38d5a4255a119c0b12
Massimo Guida <massimo.guidamusic@gmail.com>
2020-09-02 12:45:06 -0400

    added comments to API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,24 +1,26 @@
+// Import AXIOS
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  getSearchedBooks: function(title) {
-    console.log("TITLE:", title);
-    return axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:" + title.title + "&key=AIzaSyAzh3hgpTEaEpBI9WMYYqbaXI1bEb0rn4o");
-  }
+    // Gets all books
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    // Gets the book with the given id
+    getBook: function(id) {
+        return axios.get("/api/books/" + id);
+    },
+    // Saves a book to the database
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+    // Deletes the book with the given id
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    },
+    // Retrieves the books searched for by making an API call
+    getSearchedBooks: function(title) {
+        console.log("TITLE:", title);
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:" + title.title + "&key=AIzaSyAzh3hgpTEaEpBI9WMYYqbaXI1bEb0rn4o");
+    }
 };
