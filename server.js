8bad457ef559acf07e3e0e1e58f7b9a2f3a99c0d
Massimo Guida <massimo.guidamusic@gmail.com>
2020-09-02 12:40:12 -0400

    added comments for books.js and server.js variables

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Variables: require modules
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -10,21 +11,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds213079.mlab.com:13079/heroku_s4h13mt5", {useNewUrlParser: true})
-  .then(() => {
-    console.log("🗄 ==> Successfully connected to mongoDB.");
-  })
-  .catch((err) => {
-    console.log(`Error connecting to mongoDB: ${err}`);
-  });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds213079.mlab.com:13079/heroku_s4h13mt5", { useNewUrlParser: true })
+    .then(() => {
+        console.log("🗄 ==> Successfully connected to mongoDB.");
+    })
+    .catch((err) => {
+        console.log(`Error connecting to mongoDB: ${err}`);
+    });
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
